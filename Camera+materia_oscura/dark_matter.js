78e55e444fa1adc6be1c44940b27d9d672cb9ae4
leamaraa <leafabiano74@gmail.com>
2019-02-04 12:24:31 +0100

    Revert "dark matter modifica"
    This reverts commit 920dad8deb2130b4739e92f92553929f1b84c84d.

--- Camera+materia_oscura/dark_matter.js
+++ Camera+materia_oscura/dark_matter.js
@@ -20,7 +20,14 @@ function materia_oscura_draw() {
       flowfield[x][y][0] = angle;
       flowfield[x][y][1] = length;
       push();
-      stroke(255);
+      r = 255;
+      g = 255;
+      b = 255;
+      // r = random(81,255);
+      // g = 106;
+      // b = 207;
+      stroke(r, g, b);
+      //stroke(lerpColor(color('#ea0043'), color('#0fefca'), frameCount/120));
       translate(x * scl, y * scl) //così me ne fa uno ogni 20;
       rotate(angle);
       strokeWeight(0.8);
