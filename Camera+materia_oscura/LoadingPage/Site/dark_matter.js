4d424954493a9cae42a4f1387a23415e9dbda594
leamaraa <leafabiano74@gmail.com>
2019-02-05 22:56:44 +0100

    Revert "nuova eliminazione file inutili"
    This reverts commit 00e7d44cf13b22a960bfe8fa232584492876c643.

--- Camera+materia_oscura/LoadingPage/Site/dark_matter.js
+++ Camera+materia_oscura/LoadingPage/Site/dark_matter.js
@@ -4,18 +4,19 @@ function materia_oscura_setup() {
 }
 
 function materia_oscura_draw() {
-  background(0);
-  for (var y = 0; y < rows; y+=3) {
-    for (var x = 0; x < cols; x+=3) {
+
+
+  for (var y = 0; y < rows; y+=2) {
+    for (var x = 0; x < cols; x+=2) {
       var angle = module.simplex3(x / 50, y / 50, zoff) * PI * 2;
       var length = module.simplex3(x / 50 + 40000, y / 50 + 40000, zoff);
       push();
-      stroke(255);
+      stroke(124,252,0);
 
       translate(x * scl, y * scl) //così me ne fa uno ogni 20;
       rotate(angle);
-      strokeWeight(2);
-      point(scl * length, scl * length);
+      strokeWeight(2.5);
+      line(scl * length, scl * length, scl * length, scl * length);
       pop();
 
       zoff += 0.000009;
